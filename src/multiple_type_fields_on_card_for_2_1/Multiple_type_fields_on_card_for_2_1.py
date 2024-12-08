3a4ac7fe31dbcf4cb9c879fb9880bf334b20cea0
Henry Ruben Fischer <henryrubenfischer@gmail.com>
2024-12-08 16:01:49 +0100

    Try to fix #14

--- src/multiple_type_fields_on_card_for_2_1/Multiple_type_fields_on_card_for_2_1.py
+++ src/multiple_type_fields_on_card_for_2_1/Multiple_type_fields_on_card_for_2_1.py
@@ -1,4 +1,5 @@
-""" This module monkey patches the anki Reviewer to enable multiple type fields on cards. """
+""" This module monkey patches the anki Reviewer to
+enable multiple type fields on cards. """
 
 import re
 from typing import Union, Callable, Match
@@ -12,18 +13,19 @@ from aqt.utils import tr
 
 
 def myInit(self, mw):
-  oldInit(self, mw)
-  self.typeCorrect = []
+    oldInit(self, mw)
+    self.typeCorrect = []
 
 
-#This is only a constant attached to the reviewer which makes it easy to replace
+# This is only a constant attached to the reviewer
+# which makes it easy to replace
 myRevHtmlText = """
 <script>
 typetxts = document.getElementsByName("typetxt");
 function _getTypedTexts(){
     typedTexts = [];
     for (let i = 0, typedTextNode; (typedTextNode = typetxts[i]); i++) {
-        typedTexts.push(typedTextNode.value) ?? null;
+        typedTexts.push(typedTextNode.value ?? null);
     }
     return typedTexts;
 }
@@ -32,102 +34,108 @@ function _getTypedTexts(){
 
 
 def myRevHtml(self):
-  return old_revHtml(self) + myRevHtmlText
+    return old_revHtml(self) + myRevHtmlText
 
 
-#Sadly we cannot use the original code in the functions below this
+# Sadly we cannot use the original code in the functions below this
 def myTypeAnsFilter(self, buf):
-  if self.state == "question":
-    self.typeCorrect = []
-    return self.typeAnsQuestionFilter(buf)
-  else:
-    return self.typeAnsAnswerFilter(buf, 0)
+    if self.state == "question":
+        self.typeCorrect = []
+        return self.typeAnsQuestionFilter(buf)
+    else:
+        return self.typeAnsAnswerFilter(buf, 0)
 
 
 def myTypeAnsQuestionFilter(self, buf):
-  clozeIdx = None
-  m = re.search(self.typeAnsPat, buf)
-  if not m:
-    return buf
-  fld = m.group(1)
-  # if it's a cloze, extract data
-  if fld.startswith("cloze:"):
-    # get field and cloze position
-    clozeIdx = self.card.ord + 1
-    fld = fld.split(":")[1]
-  # loop through fields for a match
-  for f in self.card.note_type()["flds"]:
-    if f["name"] == fld:
-      typeCorrect = self.card.note()[f["name"]]
-      if clozeIdx:
-        # narrow to cloze
-        typeCorrect = self._contentForCloze(typeCorrect, clozeIdx)
-      self.typeFont = f["font"]
-      self.typeSize = f["size"]
-      break
-  if not typeCorrect:
-    # append none, so AnsAnswer indices don't missmatch
-    self.typeCorrect.append(None)
-    if typeCorrect is None:
-      if clozeIdx:
-        warn = tr.studying_please_run_toolsempty_cards()
-      else:
-        warn = tr.studying_type_answer_unknown_field(val=fld)
-      return re.sub(self.typeAnsPat, warn, buf)
-    else:
-      # empty field, remove type answer pattern
-      return re.sub(self.typeAnsPat, "", buf, count=1)
-  buf = re.sub(
-      self.typeAnsPat, f"""
+    clozeIdx = None
+    m = re.search(self.typeAnsPat, buf)
+    if not m:
+        return buf
+    fld = m.group(1)
+    # if it's a cloze, extract data
+    if fld.startswith("cloze:"):
+        # get field and cloze position
+        clozeIdx = self.card.ord + 1
+        fld = fld.split(":")[1]
+    if fld.startswith("nc:"):
+        self._combining = False
+        fld = fld.split(":")[1]
+    # loop through fields for a match
+    for f in self.card.note_type()["flds"]:
+        if f["name"] == fld:
+            typeCorrect = self.card.note()[f["name"]]
+            if clozeIdx:
+                # narrow to cloze
+                typeCorrect = self._contentForCloze(typeCorrect, clozeIdx)
+            self.typeFont = f["font"]
+            self.typeSize = f["size"]
+            break
+    if not typeCorrect:
+        # append none, so AnsAnswer indices don't missmatch
+        self.typeCorrect.append(None)
+        if typeCorrect is None:
+            if clozeIdx:
+                warn = tr.studying_please_run_toolsempty_cards()
+            else:
+                warn = tr.studying_type_answer_unknown_field(val=fld)
+            return re.sub(self.typeAnsPat, warn, buf)
+        else:
+            # empty field, remove type answer pattern
+            return re.sub(self.typeAnsPat, "", buf, count=1)
+    buf = re.sub(
+        self.typeAnsPat,
+        f"""
 <center>
 <input type=text id=typeans  name=typetxt onkeypress="_typeAnsPress();"
    style="font-family: '{self.typeFont}'; font-size: {self.typeSize}px;">
 </center>
-""", buf, count = 1
-  )
-  self.typeCorrect.append(typeCorrect)
-  return self.typeAnsQuestionFilter(buf)
+""",
+        buf,
+        count=1,
+    )
+    self.typeCorrect.append(typeCorrect)
+    return self.typeAnsQuestionFilter(buf)
 
 
 def myTypeAnsAnswerFilter(self, buf: str, i: int) -> str:
-  if i >= len(self.typeCorrect):
-    return re.sub(self.typeAnsPat, "", buf)
-  origSize = len(buf)
-  buf = buf.replace("<hr id=answer>", "")
-  hadHR = len(buf) != origSize
-  # munge correct value
-  expected = self.typeCorrect[i]
-  provided = self.typedAnswer[i]
-  # compare with typed answer
-  output = self.mw.col.compare_answer(expected, provided)
+    if i >= len(self.typeCorrect):
+        return re.sub(self.typeAnsPat, "", buf)
+    origSize = len(buf)
+    buf = buf.replace("<hr id=answer>", "")
+    hadHR = len(buf) != origSize
+    # munge correct value
+    expected = self.typeCorrect[i]
+    provided = self.typedAnswer[i]
+    # compare with typed answer
+    output = self.mw.col.compare_answer(expected, provided, self._combining)
 
-  # and update the type answer area
-  def repl(match: Match) -> str:
-    # can't pass a string in directly, and can't use re.escape as it
-    # escapes too much
-    s = """
+    # and update the type answer area
+    def repl(match: Match) -> str:
+        # can't pass a string in directly, and can't use re.escape as it
+        # escapes too much
+        s = """
 <div style="font-family: '{}'; font-size: {}px">{}</div>""".format(
-        self.typeFont,
-        self.typeSize,
-        output,
-    )
-    if hadHR:
-      # a hack to ensure the q/a separator falls before the answer
-      # comparison when user is using {{FrontSide}}
-      s = f"<hr id=answer>{s}"
-    return s
+            self.typeFont,
+            self.typeSize,
+            output,
+        )
+        if hadHR:
+            # a hack to ensure the q/a separator falls before the answer
+            # comparison when user is using {{FrontSide}}
+            s = f"<hr id=answer>{s}"
+        return s
 
-  buf = re.sub(self.typeAnsPat, repl, buf, count=1)
-  return self.typeAnsAnswerFilter(buf, i + 1)
+    buf = re.sub(self.typeAnsPat, repl, buf, count=1)
+    return self.typeAnsAnswerFilter(buf, i + 1)
 
 
 def myGetTypedAnswer(self):
-  self.web.evalWithCallback("_getTypedTexts()", self._onTypedAnswer)
+    self.web.evalWithCallback("_getTypedTexts()", self._onTypedAnswer)
 
 
 def myOnTypedAnswer(self, val: None) -> None:
-  self.typedAnswer = val or [""] * len(self.typeCorrect)
-  self._showAnswer()
+    self.typedAnswer = val or [""] * len(self.typeCorrect)
+    self._showAnswer()
 
 
 oldInit = Reviewer.__init__
@@ -143,29 +151,32 @@ Reviewer._getTypedAnswer = myGetTypedAnswer
 
 # ==== CLayout ====
 def myMaybeTextInput(self, txt: str, type: str = "q") -> str:
-  if "[[type:" not in txt:
-    return txt
-  origLen = len(txt)
-  txt = txt.replace("<hr id=answer>", "")
-  hadHR = origLen != len(txt)
+    if "[[type:" not in txt:
+        return txt
+    origLen = len(txt)
+    txt = txt.replace("<hr id=answer>", "")
+    hadHR = origLen != len(txt)
 
-  def answerRepl(match: Match) -> str:
-    res = self.mw.col.compare_answer("example", "sample")
-    if hadHR:
-      res = f"<hr id=answer>{res}"
-    return res
+    def answerRepl(match: Match) -> str:
+        res = self.mw.col.compare_answer("example", "sample")
+        if hadHR:
+            res = f"<hr id=answer>{res}"
+        return res
 
-  type_filter = r"\[\[type:.+?\]\]"
-  repl: Union[str, Callable]
+    type_filter = r"\[\[type:.+?\]\]"
+    repl: Union[str, Callable]
 
-  if type == "q":
-    repl = "<input id='typeans' name='typetxt' type=text value='example' readonly='readonly'>"
-    repl = f"<center>{repl}</center>"
-  else:
-    repl = answerRepl
-  out = re.sub(type_filter, repl, txt)
+    if type == "q":
+        repl = (
+            "<input id='typeans' name='typetxt'"
+            " type=text value='example' readonly='readonly'>"
+        )
+        repl = f"<center>{repl}</center>"
+    else:
+        repl = answerRepl
+    out = re.sub(type_filter, repl, txt)
 
-  return out
+    return out
 
 
 oldMaybeTextInput = CardLayout.maybeTextInput
